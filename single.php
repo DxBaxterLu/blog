5c019abe523a5c14368445c149c2a9591c78bfd9
diegomorane <diegomorane>
2022-05-20 17:52:18 -0500

    Se añadio el link a la vista de error

--- single.php
+++ single.php
@@ -1,4 +1,26 @@
 <?php
 require 'Config/Config.php';
+require 'Config/funciones.php';
+
+$conect = conect($bd_config);
+$id_article = id_article($_GET['id']);
+
+if (!$conect) {
+    header('Location: error.php');
+}
+
+if (empty($id_article)) {
+    header('Location: index.php');
+}
+
+$post = getPostsId($conect, $id_article);
+
+if (!$post) {
+    // Redirigimos al index si no hay post
+    header('Location: index.php');
+}
+
+//print_r($post);
+$post = $post[0];
 
 require 'Views/single.view.php';
