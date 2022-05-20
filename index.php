b84cb3b4cbf8334e71bf6c6d87744c247a311c2e
diegomorane <diegomorane>
2022-05-20 17:52:02 -0500

    Se añadio el link a la vista de error

--- index.php
+++ index.php
@@ -5,14 +5,14 @@ require 'Config/funciones.php';
 $conect = conect($bd_config);
 
 if (!$conect) {
-    header('Location: Views/Template/error.php');
-    //echo "Error";
+    header('Location: error.php');
+    //echo "error";
 }
 
 $posts = getPost($blog_config['post_pag'], $conect);
 
 if (!$posts) {
-    header('Location: Views/Template/error.php');
+    header('Location: error.php');
 }
 
 require 'Views/index.view.php';
