3fd97b3170b75ac36ca5c0f1002cb976eb84acb4
diegomorane <diegomorane>
2022-05-20 17:44:06 -0500

    Se bajo el numero de post por pagina de 3 a 2

--- Config/Config.php
+++ Config/Config.php
@@ -1,5 +1,5 @@
 <?php
-define('url', 'http://localhost/Blog/');
+define('url', 'http://localhost/Blog');
 
 $bd_config = array(
     'database' => 'blog',
@@ -8,8 +8,8 @@ $bd_config = array(
 );
 
 $blog_config = array(
-    'post_pag' => '3',
-    'img' => 'assets/img'
+    'post_pag' => '2',
+    'img' => 'assets/img/'
 );
 
 $blog_admin = array(
