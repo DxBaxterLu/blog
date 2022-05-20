ce9bf615d198497f48ff5e3e510de5f8fdcf3b86
diegomorane <diegomorane>
2022-05-20 17:48:57 -0500

    Se creo y se modificaron las paginaciones sobre la pag/ falta corregir errores

--- Views/paginacion.php
+++ Views/paginacion.php
@@ -1,10 +1,34 @@
 <section class="paginacion">
     <ul>
-        <li class="disabled">&laquo;</li>
-        <li><a href="#">1</a></li>
-        <li><a href="#">2</a></li>
-        <li><a href="#">3</a></li>
-        <li><a href="#">&raquo;</a></li>
+        <!-- Establecemos el numero de paginas -->
+        <?php $num_pag = num_pag($blog_config['post_pag'], $conect); ?>
+
+        <!-- Mostramos el boton para retroceder una pagina -->
+        <?php if (pag_actual() === 1) : ?>
+            <li class="disabled">&laquo;</li>
+        <?php else : ?>
+            <li><a href="index.php?p=<?php echo pag_actual() - 1; ?>">&laquo;</a></li>
+        <?php endif; ?>
+
+        <!-- Creamos un elemento li por cada pagina que tengamos -->
+        <?php for ($i = 1; $i <= $num_pag; $i++) : ?>
+            <!-- Agregamos la clase active en la pagina actual -->
+            <?php if (pag_actual() === $i) : ?>
+                <li class="active">
+                    <?php echo $i; ?>
+                </li>
+            <?php else : ?>
+                <li><a href="index.php?=<?php echo $i; ?>"><?php echo $i; ?></a></li>
+            <?php endif; ?>
+        <?php endfor; ?>
+
+        <!-- Mostramos el boton para avanzar una pagina -->
+        <?php if (pag_actual() == $num_pag) : ?>
+            <li class="disable">&raquo;</li>
+        <?php else : ?>
+            <li><a href="index.php?p=<?php echo pag_actual() + 1; ?>">&raquo;</a></li>
+        <?php endif; ?>
+
     </ul>
 
 </section>
