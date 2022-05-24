a93d9b3ce0fd821ad493904abd94630781e8d8a7
diegomorane <diegomorane>
2022-05-24 12:43:42 -0500

    Se corrigieron errores

--- Config/index.php
+++ Config/index.php
@@ -1,9 +1,13 @@
 <?php session_start();
 
+//Index del Admin
+
 require 'Config.php';
 require 'funciones.php';
 
 $conect = conect($bd_config);
+
+comprobarSession();
 
 if (!$conect) {
     header('Location: ../error.php');
