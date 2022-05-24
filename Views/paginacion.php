dd40285afb616a308cd0fc42e2e75644b0d86be9
diegomorane <diegomorane>
2022-05-24 12:44:55 -0500

    Se corrigio el error de la paginacion que no dejaba usar los numero de la pagina

--- Views/paginacion.php
+++ Views/paginacion.php
@@ -1,13 +1,14 @@
 <section class="paginacion">
     <ul>
-        <!-- Establecemos el numero de paginas -->
-        <?php $num_pag = num_pag($blog_config['post_pag'], $conect); ?>
-
+        <?php
+        # Establecemos el numero de paginas
+        $num_pag = num_pag($blog_config['post_pag'], $conect);
+        ?>
         <!-- Mostramos el boton para retroceder una pagina -->
         <?php if (pag_actual() === 1) : ?>
             <li class="disabled">&laquo;</li>
         <?php else : ?>
-            <li><a href="index.php?p=<?php echo pag_actual() - 1; ?>">&laquo;</a></li>
+            <li><a href="index.php?p=<?php echo pag_actual() - 1 ?>">&laquo;</a></li>
         <?php endif; ?>
 
         <!-- Creamos un elemento li por cada pagina que tengamos -->
@@ -18,17 +19,17 @@
                     <?php echo $i; ?>
                 </li>
             <?php else : ?>
-                <li><a href="index.php?=<?php echo $i; ?>"><?php echo $i; ?></a></li>
+                <li>
+                    <a href="index.php?p=<?php echo $i ?>"><?php echo $i; ?></a>
+                </li>
             <?php endif; ?>
         <?php endfor; ?>
 
         <!-- Mostramos el boton para avanzar una pagina -->
         <?php if (pag_actual() == $num_pag) : ?>
-            <li class="disable">&raquo;</li>
+            <li class="disabled">&raquo;</li>
         <?php else : ?>
             <li><a href="index.php?p=<?php echo pag_actual() + 1; ?>">&raquo;</a></li>
         <?php endif; ?>
-
     </ul>
-
 </section>
